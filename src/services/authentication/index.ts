c9e212a1812c6ece5d6a21f360b8434f9b43c9f8
harsh-js <harsh@dqotsolutions.com>
2022-05-23 21:26:18 +0530

    new changes

--- src/services/authentication/index.ts
+++ src/services/authentication/index.ts
@@ -1,8 +1,33 @@
-import express from "express";
+import express, { ErrorRequestHandler } from "express";
+import createHttpError from "http-errors";
+
+import mongoose from "mongoose";
+// import { DB, PORT } from "./config";
+
+import { errorHandler } from "@middleware/errorHanlder";
+import morgan from "morgan";
+import { DB, PORT } from "@config/index";
+const app = express();
+app.use(express.json());
 
 
 
+app.use(() => {
+  throw createHttpError(404, "Route not found");
+});
 
+app.use(errorHandler);
+console.log(DB)
 
-const app = express()
-
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connected to db");
+    app.listen(PORT, () => {
+      console.log(`Listening On PORT ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log('e', e)
+    throw createHttpError(501, "Unable to connect database");
+  });
